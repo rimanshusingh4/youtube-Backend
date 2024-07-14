2064a4c1dc95f89d0ccafee239cf2813ffeb2532
Rimanshu Singh <rimanshusingh4@gmail.com>
2024-07-14 10:40:36 +0530

    checking cors options

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import morgan from "morgan";
 const app = new express();
 
 const corsOptions ={
-    origin: ["https://youtube-zeta-rose.vercel.app","https://youtube-git-master-rimanshu-singhs-projects.vercel.app","https://youtube-rimanshu-singhs-projects.vercel.app"],
+    origin: ["https://youtube-zeta-rose.vercel.app","http://localhost:5173","https://youtube-git-master-rimanshu-singhs-projects.vercel.app","https://youtube-rimanshu-singhs-projects.vercel.app"],
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
